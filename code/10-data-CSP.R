2674b04ba48c1c91a2fb821c511037967984817a
Mārtiņš Liberts <martins.liberts@gmail.com>
2025-06-10 11:52:03 +0300

    CSP done

---
+++ code/10-data-CSP.R
@@ -0,0 +1,38 @@
+# CSP data
+
+# install.packages("pak")
+# pak::pkg_install("data.table")
+# pak::pkg_install("config")
+# pak::pkg_install("gt")
+# pak::pkg_install("arrow")
+# pak::pkg_install("R.utils")
+
+library(data.table)
+library(gt)
+library(purrr)
+
+# Reset
+rm(list = ls())
+gc()
+
+
+# Read CSP frame
+frame.csp <- fread(
+  file = file.path(config::get("dir.data.csp"), "Ietvars_01042025_LU.csv")
+) |> setnames(tolower)
+
+frame.csp
+
+# Replace NA with 0
+frame.csp[is.na(vc1859_sum), vc1859_sum := 0L]
+
+fwrite(
+  x = frame.csp,
+  file = "data/frame_csp.ycsv.gz",
+  yaml = TRUE
+)
+
+# arrow::write_parquet(
+#   x = frame.csp,
+#   sink = "data/frame_csp.parquet"
+# )
